ffa5d10fd9f619824c3cc21f5f74fc952cc5ebec
denkmoritz <moritzdenk0407@gmail.com>
2025-07-11 20:26:54 +0200

    inital commit

---
+++ frontend/src/App.vue
@@ -0,0 +1,10 @@
+<template>
+  <div>
+    <h1>My OSM Map</h1>
+    <MapView />
+  </div>
+</template>
+
+<script setup>
+import MapView from './components/MapView.vue'
+</script>
---
+++ frontend/src/components/MapView.vue
@@ -0,0 +1,21 @@
+<template>
+    <div id="map" style="height: 600px; width: 100%"></div>
+</template>
+
+<script setup>
+import { onMounted } from 'vue'
+import L from 'leaflet'
+
+onMounted(() => {
+
+    const map = L.map('map').setView([52.520008, 13.404954], 13)
+
+    L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    attribution:
+      '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+  }).addTo(map)
+
+  L.marker([52.52, 13.40]).addTo(map).bindPopup('Berlin!').openPopup()
+})
+</script>
+
